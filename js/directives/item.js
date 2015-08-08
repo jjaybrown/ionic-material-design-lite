edcb77b7b4819f54d1b89b4bbd9c38d79fa5493a
Jason Brown <jason.brown.delta@gmail.com>
2015-08-08 19:19:43 +0100

    added list actions added list swipe options

--- js/directives/item.js
+++ js/directives/item.js
@@ -2,15 +2,27 @@ angular.module('ionic')
     .directive('ionItem', ['$ionicPlatform', function ($ionicPlatform) {
         return {
             restrict: 'E',
-            link: function (scope, element, attrs) {
-                if(!attrs.hasOwnProperty('noRipple')) {
-                    element.addClass('mdl-js-ripple-effect');
+            compile: function (element, attrs) {
+                var upgrade = false;
+
+                // Only add ripple to list items and not dividers
+                if(!attrs.hasOwnProperty('class') || attrs.class.indexOf('item-divider') === -1) {
+                    if(!attrs.hasOwnProperty('noRipple')) {
+                        element.addClass('mdl-js-ripple-effect');
+
+                        // Add ripple container as MDL currently doesn't support lists
+                        element.append('<span class="mdl-ripple"></span>');
+                    }
+
+                    upgrade = true;
                 }
 
                 $ionicPlatform.ready(function () {
-                    // MDL should register and upgrade our element automatically,
-                    // however lets make sure it's upgraded when we compile
-                    componentHandler.upgradeElement(element[0], 'MaterialRipple');
+                    if(upgrade) {
+                        // MDL should register and upgrade our element automatically,
+                        // however lets make sure it's upgraded when we compile
+                        componentHandler.upgradeElement(element[0], 'MaterialRipple');
+                    }
                 });
             }
         }
