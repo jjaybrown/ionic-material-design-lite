58a694332897da2aa6f407e819f48cc87735a29e
Jason Brown <jason.brown.delta@gmail.com>
2015-08-20 20:25:09 +0100

    removed test tasks from npm install

--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,30 @@ gulp.task('sass', function(done) {
         .on('end', done);
 });
 
+gulp.task('test', function () {
+    // Be sure to return the stream
+    // NOTE: Using the fake './foobar' so as to run the files
+    // listed in karma.conf.js INSTEAD of what was passed to
+    // gulp.src !
+    return gulp.src('./foobar')
+        .pipe($.karma({
+            configFile: './tests/karma.conf.js',
+            action: 'run'
+        }))
+        .on('error', function (err) {
+            // Make sure failed tests cause gulp to exit non-zero
+            process.exit(1);
+        });
+});
+
+gulp.task('coverage', ['test'], function () {
+    return gulp.src('tests/coverage/**/lcov.info')
+        .pipe($.coveralls()).on('error', function (err) {
+            // Make sure failed tests cause gulp to exit non-zero
+            process.exit(1);
+        });
+});
+
 gulp.task('bump', ['dist'], function(){
     gulp.src(['./bower.json', './package.json'])
         .pipe($.bump())
