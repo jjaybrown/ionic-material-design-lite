2df2680960c724789ca713d812c2da8dbb2239ce
Jason Brown <jason.brown.delta@gmail.com>
2015-08-23 16:41:50 +0100

    [Prerelease] Bumped version to 0.3.0

--- dist/ionic.material-design-lite.js
+++ dist/ionic.material-design-lite.js
@@ -43,6 +43,41 @@ angular.module('ionic')
     }]);
 
 angular.module('ionic')
+    .directive('input', ['$ionicPlatform', '$ionicMaterialConfig', function ($ionicMaterialConfig) {
+        return {
+            restrict: 'E',
+            compile: function (element) {
+                if($ionicMaterialConfig.allPlatforms || ionic.Platform.isAndroid()) {
+                    var parent = element.parent();
+                    if(parent.hasClass('item-input')) {
+                        element.bind('focus', function () {
+                            parent.addClass('is-focused');
+                        });
+
+                        element.bind('blur', function () {
+                            parent.removeClass('is-focused');
+                            if(element.val() !== '') {
+                                parent.addClass('is-dirty');
+                            }else{
+                                parent.removeClass('is-dirty');
+                            }
+                        });
+                    }
+                }
+
+                return {
+                    post: function (scope, element, attrs) {
+                        if(attrs.hasOwnProperty('disabled') && attrs.disabled === true) {
+                            var parent = element.parent();
+                            parent.addClass('is-disabled');
+                        }
+                    }
+                };
+            }
+        }
+    }]);
+
+angular.module('ionic')
     .directive('ionItem', ['$ionicPlatform', '$ionicMaterialConfig', function ($ionicPlatform, $ionicMaterialConfig) {
         return {
             restrict: 'E',
@@ -107,6 +142,41 @@ angular.module('ionic')
                         componentHandler.upgradeElement(element[0], 'MaterialRipple');
                     });
                 }
+            }
+        }
+    }]);
+
+angular.module('ionic')
+    .directive('textarea', ['$ionicPlatform', '$ionicMaterialConfig', function ($ionicMaterialConfig) {
+        return {
+            restrict: 'E',
+            compile: function (element) {
+                if($ionicMaterialConfig.allPlatforms || ionic.Platform.isAndroid()) {
+                    var parent = element.parent();
+                    if(parent.hasClass('item-input')) {
+                        element.bind('focus', function () {
+                            parent.addClass('is-focused');
+                        });
+
+                        element.bind('blur', function () {
+                            parent.removeClass('is-focused');
+                            if(element.val() !== '') {
+                                parent.addClass('is-dirty');
+                            }else{
+                                parent.removeClass('is-dirty');
+                            }
+                        });
+                    }
+                }
+
+                return {
+                    post: function (scope, element, attrs) {
+                        if(attrs.hasOwnProperty('disabled') && attrs.disabled === true) {
+                            var parent = element.parent();
+                            parent.addClass('is-disabled');
+                        }
+                    }
+                };
             }
         }
     }]);
