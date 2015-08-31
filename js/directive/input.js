73060a719d36f8b720ba170a9d424b3ef0cb922a
Jason Brown <jason.brown.delta@gmail.com>
2015-08-31 20:56:53 +0100

    fixed input and item directives not working correctly within ng-repeats due to compile only being called once fixed styles for bar, button and list elements

--- js/directive/input.js
+++ js/directive/input.js
@@ -2,30 +2,29 @@ angular.module('ionic')
     .directive('input', ['$ionicPlatform', '$ionicMaterialConfig', function ($ionicMaterialConfig) {
         return {
             restrict: 'E',
-            compile: function (element) {
-                if($ionicMaterialConfig.allPlatforms || ionic.Platform.isAndroid()) {
-                    var parent = element.parent();
-                    if(parent.hasClass('item-input')) {
-                        element.bind('focus', function () {
-                            parent.addClass('is-focused');
-                        });
-
-                        element.bind('blur', function () {
-                            parent.removeClass('is-focused');
-                            if(element.val() !== '') {
-                                parent.addClass('is-dirty');
-                            }else{
-                                parent.removeClass('is-dirty');
-                            }
-                        });
-                    }
-                }
-
+            compile: function () {
                 return {
                     post: function (scope, element, attrs) {
-                        if(attrs.hasOwnProperty('disabled') && attrs.disabled === true) {
-                            var parent = element.parent();
-                            parent.addClass('is-disabled');
+                        var parent = element.parent();
+                        if($ionicMaterialConfig.allPlatforms || ionic.Platform.isAndroid()) {
+                            if(parent.hasClass('item-input')) {
+                                element.bind('focus', function () {
+                                    parent.addClass('is-focused');
+                                });
+
+                                element.bind('blur', function () {
+                                    parent.removeClass('is-focused');
+                                    if(element.val() !== '') {
+                                        parent.addClass('is-dirty');
+                                    }else{
+                                        parent.removeClass('is-dirty');
+                                    }
+                                });
+                            }
+
+                            if(attrs.hasOwnProperty('disabled') && attrs.disabled === true) {
+                                parent.addClass('is-disabled');
+                            }
                         }
                     }
                 };
